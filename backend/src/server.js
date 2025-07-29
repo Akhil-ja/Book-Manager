4953080eb77d68efe821318d678135f680741ea4
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-07-29 17:58:11 +0530

    feat(backend): Add mongoDB and ES modules and morgon config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,22 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// Morgan
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
